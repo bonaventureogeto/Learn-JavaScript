c219e61f996d2ee575370c0b06cd0328fabf80de
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-01-12 11:21:45 +0300

    intro to js

---
+++ script.js
@@ -0,0 +1,64 @@
+console.log("Hello, World!")
+
+// this is a single line comment
+
+/*
+    - this is a multi-line comment
+    - see!
+*/
+
+// arithmetic operators in JS
+console.log(3 + 5) //addition
+console.log(3 - 5) //subtraction
+console.log(3 * 5) // multiplication
+console.log(3 / 5) // division
+console.log(11 % 5) // modulus
+console.log(3 ** 2) // exponentiation
+
+// variables in JS
+/*
+    1. let
+    2. const 
+    3. var 
+*/
+
+// const num = 30;
+
+// console.log(num)
+
+// console.log(age)
+
+// var age = 47;
+
+// console.log(age)
+
+
+// console.log(cars)
+
+// let cars = 20;
+
+// console.log(cars)
+
+
+/* conditionals 
+    - if/else
+    - if/else if
+*/
+
+let age = 13;
+
+if (age > 18 || age < 18) {
+    console.log("You can vote")
+} else {
+    console.log("You can't vote")
+}
+
+// object
+
+var cars = {
+    lambogini: 34,
+    bmw: 37,
+    toyota: 4
+}
+
+console.log(cars.bmw)

66b5e921feef781bed3f8608edc1954f9d046778
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-01-13 11:25:15 +0300

    objects & loops

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-console.log("Welcome to this JavaScript course!") // this prints a message
+// console.log("Welcome to this JavaScript course!") // this prints a message
 
 
-// this is a single line comment
+// // this is a single line comment
 
-/*
-    - this is a multi-line comment
-    - see!
-*/
+// /*
+//     - this is a multi-line comment
+//     - see!
+// */
 
 
-// arithmetic operators in JS
+// // arithmetic operators in JS
 
-console.log(3 + 5) //addition
-console.log(3 - 5) //subtraction
-console.log(3 * 5) // multiplication
-console.log(3 / 5) // division
-console.log(11 % 5) // modulus
-console.log(3 ** 10) // exponentiation
+// console.log(3 + 5) //addition
+// console.log(3 - 5) //subtraction
+// console.log(3 * 5) // multiplication
+// console.log(3 / 5) // division
+// console.log(11 % 5) // modulus
+// console.log(3 ** 10) // exponentiation
 
 
 // variables in JS
@@ -26,18 +26,18 @@ console.log(3 ** 10) // exponentiation
     3. var 
 */
 
-const num = 30;
+// const num = 30;
 
-console.log(num)
+// console.log(num)
 
-var age = 56;
+// var age = 56;
 
-console.log(age)
+// console.log(age)
 
 
-let cars = "Mazda";
+// let cars = "Mazda";
 
-console.log(cars)
+// console.log(cars)
 
 
 /* conditionals 
@@ -45,33 +45,64 @@ console.log(cars)
     - if/else if
 */
 
-let amount = 50;
-let min_amount = 50;
+// let amount = 50;
+// let min_amount = 50;
 
-if (amount > 50) {
-    console.log("You can withdraw amount")
-} else if (amount == min_amount) {
-    console.log("You only have 50 in your account")
-} else {
-    console.log("You have insufficient funds and can't withdraw")
-}
+// let car = "Tesla";
 
 
+// if (amount > 50) {
+//     console.log("You can withdraw amount")
+// } else if (amount == min_amount) {
+//     console.log(car)
+//     console.log("You only have 50 in your account")
+// } else {
+//     console.log("You have insufficient funds and can't withdraw")
+// }
 
-let age = 13;
 
-if (age > 18 || age < 18) {
-    console.log("You can vote")
-} else {
-    console.log("You can't vote")
-}
+
+// let age = 13;
+
+// if (age > 18 || age < 18) {
+//     console.log("You can vote")
+// } else {
+//     console.log("You can't vote")
+// }
 
 // // object
 
 // var cars = {
 //     lambogini: 34,
 //     bmw: 37,
-//     toyota: 4
+//     toyota: 4,
+//     Tesla: "ModelS",
+//     VW: "Sedan",
+//     bugatti: 23
 // }
 
-// console.log(cars.bmw)
+// // for--in loop
+
+// for (var key in cars) {
+//     console.log(key)
+// }
+
+// while loop
+
+let i = 0;
+
+while (i < 11) {
+    console.log("The number is " + i);
+    i++; // same as i = i + 1
+}
+
+// for loop & break vs continue
+
+// for (var i = 0; i < 11; i++) {
+
+//     if (i == 8) {
+//         continue;
+//     }
+
+//     console.log("The number is " + i)
+// }

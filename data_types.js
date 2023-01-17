dedcf72de40a2f7d04a8898d769ac0036381695f
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-01-17 10:42:36 +0300

    data types

---
+++ data_types.js
@@ -0,0 +1,17 @@
+// strings - text, characters
+
+console.log("Hello, World!")
+
+console.log('Hello, World!')
+
+let number = "12345"
+
+let num = parseFloat(number)
+
+console.log(typeof(num))
+
+let school = "shimo la tewa"
+
+console.log(school.toUpperCase())
+
+console.log(school.toLowerCase())

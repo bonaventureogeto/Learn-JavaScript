5f449a655ce72cbedeeaafc704389c3148970bf5
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-01-25 09:36:00 +0300

    methods to add, edit, and delete tasks

--- challenge/script.js
+++ challenge/script.js
@@ -41,3 +41,45 @@ function TaskController(taskList) {
         this.taskList.deleteTask(index);
     }
 }
+
+// create instances of the Task, TaskList, and TaskController objects and use their methods to add, edit, and delete tasks
+let taskList = new TaskList();
+let taskController = new TaskController(taskList);
+
+document.getElementById("add-task-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    let title = document.getElementById("title").value;
+    let description = document.getElementById("description").value;
+    let status = document.getElementById("status").value;
+    taskController.addTask(title, description, status);
+    updateTaskList();
+});
+
+function updateTaskList() {
+    let taskListElement = document.getElementById("task-list");
+    taskListElement.innerHTML = "";
+    for (let i = 0; i < taskList.tasks.length; i++) {
+        let task = taskList.tasks[i];
+        let taskElement = document.createElement("div");
+        taskElement.classList.add("task");
+        taskElement.innerHTML = `
+            <h3 class="task-title">${task.title}</h3>
+            <p class="task-description">${task.description}</p>
+            <p class="task-status">${task.status}</>
+            <button class="edit-button">Edit</button>
+            <button class="delete-button">Delete</button>
+        `;
+        taskListElement.appendChild(taskElement);
+
+        taskElement.querySelector(".edit-button").addEventListener("click", function () {
+            // code to open a form to edit the task
+        });
+
+        taskElement.querySelector(".delete-button").addEventListener("click", function () {
+            taskController.deleteTask(i);
+            updateTaskList();
+        });
+    }
+}
+
+

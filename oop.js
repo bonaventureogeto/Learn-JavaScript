a8d3feed558e5e4f7643f475f373ac98c82d068a
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-03-21 12:08:06 +0300

    encapsulation in JavaScript

--- oop.js
+++ oop.js
@@ -20,3 +20,39 @@ Person.prototype = {
 }
 let p3 = new Person('John', 20, 20);
 console.log(Person.prototype);
+
+
+// Encapsulation in JavaScript
+ function BankAccount(balance){
+    let _balance = balance
+
+    this.getBalance = function(){
+        return _balance
+    }
+
+    this.deposit = function(amount){
+        _balance += amount
+        return _balance
+    }
+
+    this.withdraw = function(amount){
+        if(_balance >= amount){
+            return _balance -= amount
+        } else{
+            console.log("Insufficient funds")
+        }
+    }
+
+ }
+
+let account = new BankAccount(10000)
+
+console.log(account.getBalance())
+
+account.deposit(99999)
+
+console.log(account.getBalance())
+
+account.withdraw(1380)
+
+console.log(account.getBalance())

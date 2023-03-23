3d645492a23556f34068e841631c07402aac6a34
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-03-23 10:51:38 +0300

    event-handlers

--- dom-project/app.js
+++ dom-project/app.js
@@ -12,3 +12,9 @@ const cartButton = document.getElementById("button");
 
 const itemTag = document.getElementsByTagName("h3")[0];
 
+redColor.addEventListener("click", function(){
+    cartButton.style.backgroundColor = "red";
+    itemTag.style.backgroundColor = "red";
+    imageCard.style.backgroundImage = 'url("benz-3.webp")';
+});
+

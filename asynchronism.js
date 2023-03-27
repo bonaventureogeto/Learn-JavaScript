23c0afe8ebe5540cdf4efd385a5cf088df03ca7a
Bonaventure <bonaogeto1@gmail.com>
2023-03-27 11:52:42 +0300

    asynchrous js

---
+++ asynchronism.js
@@ -0,0 +1,7 @@
+console.log("task 1");
+
+setTimeout(() => console.log("task 2"), 0);
+
+// setTimeout(fxn, time) is a function that takes a function and a time in milliseconds
+
+console.log("task 3");

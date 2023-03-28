143c8c9cb5799f424e61339b09d70ddf8ac6e18f
Bonaventure <bonaogeto1@gmail.com>
2023-03-28 09:53:36 +0300

    updates

--- dom/app.js
+++ dom/app.js
@@ -1,3 +1,9 @@
+let title1 = document.createElement("h1"); // creates a new element with the given tag name
+
+title1.textContent = "Africa is a beautiful continent";
+document.body.appendChild(title1); // appends the given element to the end of the list of children of the given parent node
+
+
 let title = document.getElementById("title"); // returns the element with the given ID
 
 title.innerHTML = "The most common way to access a DOM element is by its ID attribute. You can use the document.getElementById() method to get the element with a specific ID:";
@@ -6,16 +12,27 @@ title.innerHTML = "The most common way to access a DOM element is by its ID attr
 
 let heading = document.getElementsByTagName("h3"); // returns an array-like collection of elements with the given tag name
 
-heading[0].innerHTML = "The most common way to access a DOM element is by its ID attribute. You can use the document.getElementById() method to get the element with a specific ID:";
+// heading[0].innerHTML = "The most common way to access a DOM element is by its ID attribute. You can use the document.getElementById() method to get the element with a specific ID:";
+
+for(let i = 0; i < heading.length; i++) {
+  heading[i].innerHTML = "The most common way to access a DOM element is by its ID attribute. You can use the document.getElementById() method to get the element with a specific ID:";
+}
 
 let something = document.getElementsByClassName("something"); // returns an array-like collection of elements with the given class name
 
-something[0].textContent = "You can also access elements by their class name using the document.getElementsByClassName() method. This method returns an array-like collection of elements with the given class name";
+for(let i = 0; i < something.length; i++) {
+  something[i].innerHTML = "You can also access elements by their class name using the document.getElementsByClassName() method. This method returns an array-like collection of elements with the given class name";
+}
+
+
+// something[0].textContent = "You can also access elements by their class name using the document.getElementsByClassName() method. This method returns an array-like collection of elements with the given class name";
 
 let button = document.getElementsByTagName("button"); // returns an array-like collection of elements with the given tag name
 
 button[0].onclick = function () {
-  alert("You clicked me!");
+  this.innerHTML = "You clicked me!";
+  this.style.backgroundColor = "red";
+  // alert("You clicked me!");
 };
 
 let myDiv = document.getElementsByClassName("myDiv"); // returns an array-like collection of elements with the given class name
@@ -24,7 +41,4 @@ for (let i = 0; i < myDiv.length; i++) {
   myDiv[i].style.backgroundColor = "green";
 }
 
-let title1 = document.createElement("h1"); // creates a new element with the given tag name
 
-title1.textContent = "Africa is a beautiful continent";
-document.body.appendChild(title1); // appends the given element to the end of the list of children of the given parent node

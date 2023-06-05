02d312ba8e7c8d20f58fdb49241362c335a5196d
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-06-05 19:41:46 +0300

    intro to js

--- html-css-intro/script.js
+++ html-css-intro/script.js
@@ -6,55 +6,70 @@
 /*
     This is a multi-line comment
     the following code will print a message to the console
+    this is also a comment
 */
 
-// console.log("Welcome to Kenya. The cradle land of corruption.");
+console.log("Heyy, I think you're really cool!");
+console.log("I hate you alot.");
+console.log("Shoot your shot!");
 
-var name = "Chris";
+console.log(23486385); // this is a number
+console.log("23486385"); // this is a string
 
-var age = 30;
+console.log(true); // this is a boolean
+console.log(false); // this is a boolean
 
-// console.log("My name is Barrack Obama and I am 25 years old.")
+var name = "Mike Sonko"; // variable declaration and initialization
 
-// console.log("My name is " + name + " and I am " + age + " years old.");
+console.log(name); // print the value of the variable name to the console
 
-// Declare two variables, a first_name and a last_name, and then concatenate them together
+var age = 25; // variable declaration and initialization
+
+console.log(age); // print the value of the variable age to the console
+
+var town = "Melborne"; // variable declaration and initialization
+
+console.log("My name is James Bron from New York City and I am 30 centuries old.")
+
+console.log("My name is " + name + " from " + town + " and I'm " + age + " centuries old.")
+
+// Simple task: Declare two variables, a first_name and a last_name, and then concatenate them together and console log the result
 
 // addition
-// var sum = 2 + 2;
-// console.log(sum);
+var sum = 2 + 2;
+console.log(sum);
 
-// // subtraction
-// var difference = 45 - 10;
-// console.log(difference);
+// subtraction
+var difference = 45 - 10;
+console.log(difference);
 
-// // multiplication
-// var product = 8 * 8;
-// console.log(product);
+// multiplication
+var product = 8 * 8;
+console.log(product);
 
-// // division
-// var quotient = 66 / 33;
-// console.log(quotient);
+// division
+var quotient = 66 / 33;
+console.log(quotient);
 
-// // modulus
-// var remainder = 10 % 3;
-// console.log(remainder);
+// modulus
+var remainder = 10 % 3;
+console.log(remainder);
 
-// var x = 10;
-// var y = 5;
+var x = 10;
+var y = 5;
 
-// console.log(x == y);
-// console.log(x != y);
-// console.log(x > y);
-// console.log(x < y);
-// console.log(x >= y);
-// console.log(x <= y);
+console.log(x == y); // equality operator
+console.log(x != y); // inequality operator
+console.log(x > y); // greater than operator
+console.log(x < y); // less than operator
+console.log(x >= y); // greater than or equal to operator
+console.log(x <= y); // less than or equal to operator
 
 // logical operators
 
 var x = true;
 var y = false;
 
-console.log(x && y);
-console.log(x || y);
-console.log(!x);
+console.log(x && y); // and operator
+console.log(x || y); // or operator
+console.log(!x); // not/negation operator

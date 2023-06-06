69d532b2f34ce629ef4367b3b1056011d3b8896b
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-06-06 11:19:18 +0300

    calculator assignment & day-2

---
+++ html-css-intro/calculator.js
@@ -0,0 +1,13 @@
+var num1 = 10;
+var num2 = 20;
+
+var add_result = num1 + num2;
+var sub_result = num1 - num2;
+var mul_result = num1 * num2;
+var div_result = num1 / num2;
+
+console.log("Addition result is: " + add_result);
+console.log("Subtraction result is: " + sub_result);
+console.log("Multiplication result is: " + mul_result);
+console.log("Division result is: " + div_result);
+
---
+++ html-css-intro/day2.js
@@ -0,0 +1,20 @@
+// console.log(name); // undefined
+
+// var name = "John";
+
+// console.log(name);
+
+// ES 6: let and const
+// console.log(name); // ReferenceError: Cannot access 'name' before initialization
+
+let name = "John";
+
+name = "Mark"; // overwrites the value of name ("John")
+
+console.log(name);
+
+const age = 20; // cannot be reassigned - immutable - constant
+
+age = 30; // TypeError: Assignment to constant variable.
+
+console.log(age);

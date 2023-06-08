a3b73c93bbda4fcab0364b6f0648dc90500fffa6
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-06-08 21:46:50 +0300

    fib updates

--- fibonacci.js
+++ fibonacci.js
@@ -1,4 +1,16 @@
-// fibonacci
+/**
+    Challenge: Fibonacci Sequence
+        Create a function called fibonacci_sequence that takes in an integer n as its parameter 
+        and returns a list of the first n numbers in the Fibonacci sequence.
+        The Fibonacci sequence is a series of numbers in which each number is 
+        the sum of the two preceding ones. The sequence starts with 0 and 1, 
+        and each subsequent number is the sum of the previous two.
+        Example input/output:
+        fibonacci_sequence(5) => [0, 1, 1, 2, 3]
+        fibonacci_sequence(10) => [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+        Hint: You can use a recursive function to generate the sequence,
+        and utilize the concept of function scope to keep track of the previous two numbers.
+ */
 
 function fibonacci(number){
 

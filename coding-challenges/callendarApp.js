00fb12df4fb60c065c2b66a1dfe0b27d51bbdd42
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-06-10 13:05:36 +0300

    js coding challenges

---
+++ coding-challenges/callendarApp.js
@@ -0,0 +1,3 @@
+let calendar = [
+    {title: "Event 1", date: "2020-01-01", location: "Lavington", attendees: {"Grace", "Fathia", "Ken"}, },
+]
---
+++ coding-challenges/groupBy.js
@@ -0,0 +1,10 @@
+function groupBy(obj_array, property_name) {
+    
+}
+
+// array of objects
+let example2 = [
+    { name: "John", hobby: "soccer" },
+    { name: "Mary", hobby: "swimming" },
+    { name: "Joe", hobby: "soccer" },
+]

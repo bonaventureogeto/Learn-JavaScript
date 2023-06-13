70f6f970567334bdef2843e07646e937ea4373ab
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-06-13 08:55:04 +0300

    updates on DOM

--- dom-2/script.js
+++ dom-2/script.js
@@ -1,46 +1,42 @@
-const redColor = document.querySelector(".red")
+const redColor = document.querySelector(".red");
 
-const blackColor = document.querySelector(".black")
+const blackColor = document.querySelector(".black");
 
-const imageCard = document.querySelector(".product-image")
+const grayColor = document.getElementsByClassName("gray");
 
-const feedbackBtn = document.querySelector(".feedback")
+const description = document.getElementsByClassName("product-description");
 
-const grayColor = document.getElementsByClassName("gray")
+const imageCard = document.querySelector(".product-image");
 
-const cartButton = document.getElementById("button")
+const feedbackBtn = document.querySelector(".feedback");
 
-const itemTag = document.getElementsByTagName("h3")[0]
+const cartButton = document.getElementById("button");
+
+const itemTag = document.getElementsByTagName("h3")[0];
 
 redColor.addEventListener("click", () => {
-    cartButton.style.backgroundColor = "red",
-    itemTag.style.backgroundColor = "red",
-    imageCard.style.backgroundImage = "url('./images/red-benz.jpg')"
-})
+    cartButton.style.backgroundColor = "red";
+    itemTag.style.backgroundColor = "red";
+    imageCard.style.backgroundImage = "url('./images/red-benz.jpg')";
+    // description[0].style.backgroundColor = "red";
+});
 
 blackColor.addEventListener("click", () => {
-    cartButton.style.backgroundColor = "black",
-    itemTag.style.backgroundColor = "black",
-    imageCard.style.backgroundImage = "url('./images/black-benz.jpg')"
-})
+    cartButton.style.backgroundColor = "black";
+    itemTag.style.backgroundColor = "black";
+    imageCard.style.backgroundImage = "url('./images/black-benz.jpg')";
+    // description[0].style.backgroundColor = "black";
+});
 
 grayColor[0].addEventListener("click", () => {
-    cartButton.style.backgroundColor = "gray",
-    itemTag.style.backgroundColor = "gray",
-    imageCard.style.backgroundImage = "url('./images/gray-benz.png')"
-})
-
-// const cart = () => {
-//     cartButton.style.display = "none",
-//     feedbackBtn.style.display = "block"
-// }
+    cartButton.style.backgroundColor = "gray";
+    itemTag.style.backgroundColor = "gray";
+    imageCard.style.backgroundImage = "url('./images/gray-benz.png')";
+    // description[0].style.backgroundColor = "gray";
+});
 
 cartButton.addEventListener("click", () => {
-    cartButton.style.display = "none",
+    cartButton.style.display = "none";
     feedbackBtn.style.display = "block";
-})
+});
 
-feedbackBtn.addEventListener("click", () => {
-    cartButton.style.display = "block",
-    feedbackBtn.style.display = "none";
-})

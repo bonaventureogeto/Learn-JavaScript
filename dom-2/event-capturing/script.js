6b9d5715ab560831ee0a9e6b93562ff69bbef7bb
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-06-13 11:19:31 +0300

    stopping event propagation

--- dom-2/event-capturing/script.js
+++ dom-2/event-capturing/script.js
@@ -4,10 +4,12 @@ const btn = document.getElementById('btn');
 
 outer.addEventListener('click', () => {
     alert('outer', event.eventPhase);
+    event.stopPropagation();
 }, true);
 
 inner.addEventListener('click', () => {
     alert('inner', event.eventPhase);
+
 }, true);
 
 btn.addEventListener('click', () => {

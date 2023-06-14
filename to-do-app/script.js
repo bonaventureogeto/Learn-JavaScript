c490ef92dbc18ee262d8292d9c299019131fcd80
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-06-14 18:50:52 +0300

    updates

--- to-do-app/script.js
+++ to-do-app/script.js
@@ -4,21 +4,36 @@ const addTaskBtn = document.getElementById("add-task-btn");
 
 const taskList = document.getElementById("task-list");
 
-addTaskBtn.addEventListener('click', function () {
-  const taskText = taskInput.value;
+const clearTaskBtn = document.getElementById("clear-task-btn");
 
+
+// adds a task item to the task list
+addTaskBtn.addEventListener('click', function () {
+
+  // gets the task input value
+  let taskText = taskInput.value;
+
+  // checks if the task input is not empty
   if (taskText.trim() !== '') {
     const taskItem = document.createElement('li');
+
+    // adds the task text to the task item
     taskItem.innerText = taskText;
 
     taskItem.addEventListener('click', function () {
       taskItem.remove();
-      // alert("Task Item REMOVED!!")
     });
 
+    // removes the task item when clicked
+    clearTaskBtn.addEventListener('click', function () {
+      taskItem.remove();
+    });
+
+    // adds the task item to the task list
     taskList.appendChild(taskItem);
 
-    taskInput.value = "";
+    // clears the task input
+    taskInput.value = '';
+
   }
-  
 });

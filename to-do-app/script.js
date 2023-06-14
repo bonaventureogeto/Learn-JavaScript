bf9ab6662e4abf72025d39282b3f24c38bfe3821
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-06-14 10:53:49 +0300

    to-do list app functionality

--- to-do-app/script.js
+++ to-do-app/script.js
@@ -1,22 +1,24 @@
-const taskInput = document.getElementById("task-input");
+const taskInput = document.getElementById('task-input');
+
 const addTaskBtn = document.getElementById("add-task-btn");
+
 const taskList = document.getElementById("task-list");
 
-addTaskBtn.addEventListener("click", function () {
+addTaskBtn.addEventListener('click', function () {
   const taskText = taskInput.value;
-  if (taskText.trim() !== "") {
-    const taskItem = document.createElement("li");
+
+  if (taskText.trim() !== '') {
+    const taskItem = document.createElement('li');
     taskItem.innerText = taskText;
-    taskItem.addEventListener("click", function () {
+
+    taskItem.addEventListener('click', function () {
       taskItem.remove();
+      // alert("Task Item REMOVED!!")
     });
+
     taskList.appendChild(taskItem);
+
     taskInput.value = "";
   }
-});
-
-taskList.addEventListener("click", function (event) {
-  if (event.target.tagName.toLowerCase() === "li") {
-    event.target.remove();
-  }
+  
 });

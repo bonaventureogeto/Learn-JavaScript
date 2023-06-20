db423ef17168b08908db01f01a276f664c820918
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-06-20 09:48:24 +0300

    async await

--- Asynchronous-JS/example/script.js
+++ Asynchronous-JS/example/script.js
@@ -32,19 +32,19 @@ const myPromise = new Promise((resolve, reject) => {
 
 // console.log(myPromise);
 
-myPromise.then((result) => {
-    console.log(result);
-})
-    .catch((error) => {
-        console.log(error);
-    })
-    .finally(() => {
-        console.log('finally');
-    });
+// myPromise.then((result) => {
+//     console.log(result);
+// })
+//     .catch((error) => {
+//         console.log(error);
+//     })
+//     .finally(() => {
+//         console.log('finally');
+//     });
 
 // fetch API
 
-fetch("https://fakestoreapi.com/products")
+fetch("https://fakestoreapi.com/products/1")
     .then((response) => response.json())
     .then((data) => {
         try {
@@ -58,13 +58,21 @@ fetch("https://fakestoreapi.com/products")
     });
 
 
-let promise1 = fetch("https://jsonplaceholder.typicode.com/posts/1");
-let promise2 = fetch("https://jsonplaceholder.typicode.com/posts/2");
-let promise3 = fetch("https://jsonplaceholder.typicode.com/posts/3");
+// let promise1 = fetch("https://jsonplaceholder.typicode.com/posts/1");
+// let promise2 = fetch("https://jsonplaceholder.typicode.com/posts/2");
+// let promise3 = fetch("https://jsonplaceholder.typicode.com/posts/3");
 
-Promise.all([promise1, promise2, promise3])
-    .then((responses) => {
-        console.log(responses);
-    })
+// Promise.all([promise1, promise2, promise3])
+//     .then((responses) => {
+//         console.log(responses);
+//     })
 
-    // async await
+// async await
+    
+async function getData() {
+    const response = await fetch("https://api.github.com/users/bonaventureogeto");
+    const data = await response.json();
+    console.log(data);
+}
+
+getData();

0c258068faf1ca9c99321dca14b60f025d1d1f23
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-07-22 11:54:58 +0300

    fibonacci sequence

--- fibonacci.js
+++ fibonacci.js
@@ -12,9 +12,15 @@
         and utilize the concept of function scope to keep track of the previous two numbers.
  */
 
-function fibonacci(number){
+
+function fibonacci(number) {
+    let fib = [0, 1]; // Initialize array!
+    for (let i = 2; i < number; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    console.log(fib);
 
 }
 
-fibonacci(11) // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+fibonacci(1000) // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 

d0048610e901a6a2f5bcc1f5e6920c4514dc936f
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-09-12 11:29:21 +0300

    intro to JS

---
+++ Introduction to JavaScript/variables.js
@@ -0,0 +1,31 @@
+// Introduction to Data Types, Variables, and operators in JavaScript
+
+// Data Types in JavaScript
+console.log("Hello, World!"); // hello world is a string
+console.log(237); // 237 is a number
+console.log("237"); // 237 is string
+console.log(true); // this is a boolean
+console.log(false); // this is a boolean as well
+
+/*
+    Varibales in JavaScript:
+    Variables are used to store data values.
+    In JavaScript, we can declare variables using the var, let, and const keyword.
+*/
+
+let name; // variable declaration
+
+name = "Jane Doe"; // variable initialization
+
+// console.log(name);
+
+// mutability - ability to change a variable/value
+
+let num = 390.45; // variable declaration and initialization
+num = 234; // overriding
+
+console.log(num);
+
+const num1 = 456; // const is immutable
+
+console.log(num1);

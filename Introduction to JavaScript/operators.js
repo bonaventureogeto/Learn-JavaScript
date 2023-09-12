804c1e6595da684c422785189070c9cc82caa72d
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-09-12 12:06:52 +0300

    operators in JS

---
+++ Introduction to JavaScript/operators.js
@@ -0,0 +1,39 @@
+// Operators in JavaScript
+
+// Arithmetic operators
+let number1 = 300;
+
+let number2 = 300;
+
+let sum = number1 + number2; // addition
+
+let diff = number2 - number1; // subtraction
+
+let multiplication = number2 * number1; // multiplication
+
+let division = number2 / number1; // division
+
+let modulus = number2 % number1; // modulus
+
+console.log(sum);
+console.log(diff);
+console.log(multiplication);
+console.log(division);
+console.log(modulus);
+
+
+// Comparison operators
+
+let x = 5; // number
+let y = "5"; // string
+
+console.log(x == y); // true
+console.log(x === y); // false
+console.log(x != y); // true
+console.log(x > y); // true
+console.log(x < y); // false
+console.log(x >= y); // true
+console.log(x <= y); // false
+
+// logical operators
+

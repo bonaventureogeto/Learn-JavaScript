2a0c0bd894c44f95c1d2fbe712150e8dd79109ff
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-09-13 11:26:12 +0300

    array methods

--- Introduction to JavaScript/arrays.js
+++ Introduction to JavaScript/arrays.js
@@ -12,6 +12,28 @@ let items = [
   [1, "Toyota", 3, "BMW", true, false],
 ]; // array of arrays
 
-console.log(numbers[6]);
+
+// Accessing array elements
+console.log(numbers[5]);
 
 console.log(items[2][1]);
+
+// Array methods
+
+numbers.push(10); // adds an element to the end of the array
+
+numbers.pop(); // removes an element from the end of the array 
+
+numbers.unshift(0); // adds an element to the beginning of the array
+
+numbers.shift(); // removes an element from the beginning of the array
+
+numbers.reverse(); // reverses the array
+
+numbers.sort(); // sorts the array in ascending order
+
+let numbers_slice = numbers.slice(2, 7); // returns a new array from index 2 to 7
+
+console.log(numbers_slice);
+
+console.log(numbers);

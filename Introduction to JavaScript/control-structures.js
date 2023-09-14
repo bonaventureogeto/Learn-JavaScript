3086ea2b8742e7b7f2314c6afb7b94dbaa4666e6
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-09-14 15:12:40 +0300

    control structures

---
+++ Introduction to JavaScript/control-structures.js
@@ -0,0 +1,14 @@
+// if statements
+
+let age = 17;
+
+if (age >= 18 || age <=120) {
+    console.log("You're eligible to VOTE");
+} else if (age >= 65) {
+    console.log("Retirement Age!")
+} else {
+    console.log("Enter the correct age.")
+}
+
+// write a JavaScript program that checks for someone's account and tells them
+// they're broke if they have less than $10.

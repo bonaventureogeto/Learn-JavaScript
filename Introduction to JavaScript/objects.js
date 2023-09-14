dc7a94e69b03dc5e625eea24351d17c3b94e89fd
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-09-14 14:15:01 +0300

    objects and control structure

--- Introduction to JavaScript/objects.js
+++ Introduction to JavaScript/objects.js
@@ -27,3 +27,36 @@ console.log(person.family.sisters.sister1); // dot notation
 console.log(person.hobbies[0]); // accesing values inside a list/array
 
 console.log(person["family"]["sisters"]["sister1"]); // using index
+
+
+// list of objects
+
+let countries = [
+    {
+        name: "Kenya",
+        continent: "Africa",
+        population: "50M"
+    },
+    {
+        name: "India",
+        continent: "Asia",
+        population: "1.4B"
+    },
+    {
+        name: "Switzerland",
+        continent: "Europe",
+        population: "8.5M"
+    }
+]
+
+console.log(countries[0].population);
+
+// Write a JavaScript program that takes in an object representing a person1 and 
+// returns a string of their full name.
+
+let person1 = {
+    firstName: "John",
+    lastName: "Doe"
+}
+
+console.log(person1.firstName + " " + person1.lastName);

c9aa65f410c13eaa62adf4953c6b818ec5f04598
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-09-18 22:34:52 +0300

    intro to the JS DOM

---
+++ javascriptDOM/script.js
@@ -0,0 +1,21 @@
+const box = document.querySelector(".box");
+const para1 = document.querySelector("#box-1");
+
+const container = document.getElementsByClassName("container");
+const paragraph = document.getElementById("box-1");
+
+const headings = document.querySelector("h2");
+
+const imageCard = document.getElementsByTagName("item-image")[0];
+const button = document.getElementsByTagName("button")[0];
+
+/*
+    1. Event
+    2. Event listener
+    3. Event handler
+*/
+
+button.addEventListener('click', () => {
+    para1.style.backgroundColor = "violet";
+    // imageCard.style.backgroundImage = 'url("1.png")';
+});

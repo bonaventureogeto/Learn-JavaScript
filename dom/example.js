e6c494b80885a0a707a97a1bb69b66d7af80ea3b
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-09-19 12:03:30 +0300

    updates

--- dom/example.js
+++
@@ -1,5 +0,0 @@
-let cardColor = document.querySelectorAll(".card"); // returns a static (not live) NodeList representing a list of the document's elements that match the specified group of selectors
-
-for (let i = 0; i < cardColor.length; i++){
-    cardColor[i].style.backgroundColor = "green";
-}

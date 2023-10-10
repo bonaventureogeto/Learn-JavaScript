71bb31e600ab49a52aa3db5e9e7d1561e8e4cccb
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-10-10 20:59:35 +0300

    updates

--- dom/todo App/script.js
+++ dom/todo App/script.js
@@ -1,7 +1,13 @@
 const myTask = document.getElementsByTagName("li");
 
+const myTasks = document.getElementById('tasks');
+
+const myDesc = document.getElementById('desc');
+
 let tasks = ['Laundry', 'Cooking', 'Shopping'];
 
 myTask[0].textContent = tasks[0]
 myTask[1].textContent = tasks[1]
 myTask[2].textContent = tasks[2];
+
+myDesc.innerHTML = 'Organize your work today'
